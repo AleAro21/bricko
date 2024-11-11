5303737dffe2f13d9a3d4e8713d6b27552675530
AleAro <aarouesty@pastpost.com>
2024-11-11 16:41:39 +0100

    #2892 changed route so that screen is removed from flow for now

--- app/start/assets/page.jsx
+++ app/start/assets/page.jsx
@@ -3,7 +3,7 @@ import Start from "@/components/common/Start";
 export default function page() {
   return (
     <main className='container w-3/4 mx-auto flex flex-col min-h-screen'>
-    <Start heading={'¿Todo tu patrimonio está ubicado en México?'} linkNo={'/start/benefits?recommendation=telephonic'} linkyes={'/start/business'} titleNo={'Esto incluye cuentas bancarias, propiedades, acciones y participaciones fuera de México.'} titleYes={'Todo mi patrimonio se encuentra en México.'} />
+    <Start heading={'¿Todo tu patrimonio está ubicado en México?'} linkNo={'/start/benefits?recommendation=telephonic'} linkyes={'/start/benefits?recommendation=online'} titleNo={'Esto incluye cuentas bancarias, propiedades, acciones y participaciones fuera de México.'} titleYes={'Todo mi patrimonio se encuentra en México.'} />
     </main>
   );
 }
--- components/common/WaitingList.jsx
+++ components/common/WaitingList.jsx
@@ -47,24 +47,8 @@ const WaitingList = () => {
             </form>
           </div>
         </div>
-        <div className='w-full flex items-center justify-between'>
-          <div
-            onClick={() => router.back()}
-            className='flex items-center text-[14px] font-[500] gap-2  pt-1 text-[#9999] cursor-pointer border-b border-transparent hover:border-[#9999] transiton-all delay-150'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 448 512'
-              height={'14px'}
-              width={'14px'}
-            >
-              <path
-                fill='#9999'
-                d='M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z'
-              />
-            </svg>
-            Regresar
-          </div>
+        <div className='w-full flex items-end justify-end'>
+          
           <button className='text-[14px] text-[#000000] font-[600] bg-[#FFDF4E] px-4 py-2 rounded-[100px] uppercase'>
             Enviar
           </button>
